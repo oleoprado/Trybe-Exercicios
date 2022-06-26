dcc7d85cd403e0626d0def6c5dc6fb5d1b230a3f
Leonardo Prado <leonardo.rprado@gmail.com>
2022-06-26 15:42:43 +0100

    resolvido exercicio 3

--- Fundamentos/Bloco-04-intro-javaScript/dia-3-logica-algoritmo/exercicios.js
+++ Fundamentos/Bloco-04-intro-javaScript/dia-3-logica-algoritmo/exercicios.js
@@ -7,3 +7,25 @@ for (let index = fatorial -1; index > 0; index -= 1) {
   resultado *= index;
 }
 console.log(resultado)
+
+//2-desenvolva um algoritmo que é capaz de inverter uma palavra
+let word = 'paralelepipedo';
+let invertWord = "";
+
+for (let i = word.length -1; i >= 0; i -= 1) {
+  invertWord += word[i];
+}
+console.log(invertWord);
+
+//3-Escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor. Considere o número de caracteres de cada palavra.
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maiorPalavra = array[0]; 
+let menorPalavra = array[0]; 
+
+for (let index = 0; index < array.length; index +=1) {
+  if (array[index].length > maiorPalavra.length) {
+    maiorPalavra = array[index];
+  }
+}
+
+console.log(maiorPalavra);
