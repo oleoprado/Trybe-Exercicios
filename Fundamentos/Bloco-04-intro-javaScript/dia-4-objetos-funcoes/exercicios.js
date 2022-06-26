ed79be5dc1d156f68890e7334223e45546219073
Leonardo Prado <leonardo.rprado@gmail.com>
2022-06-26 23:35:37 +0100

    resolvido os exericios do 5 ao 8 e finalizado os exercicios do dia

--- Fundamentos/Bloco-04-intro-javaScript/dia-4-objetos-funcoes/exercicios.js
+++ Fundamentos/Bloco-04-intro-javaScript/dia-4-objetos-funcoes/exercicios.js
@@ -16,8 +16,46 @@ for (let key in info) {
 }
 
 //4 - Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto
+
 for (let key in info) {
-  console.log(info[key]);
+  // console.log(info[key]);
+}
+//5-Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves),Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves.
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim',
+};
+
+for (let keyMain in info) {
+  if (keyMain === 'recorrente' && info[keyMain] === 'Sim' && info2[keyMain] === 'Sim') {
+    console.log('Ambas recorrentes');
+  } else {
+    console.log(info[keyMain] + ' ' + info2[keyMain]);
+  }
 }
 
-//5-
+// Usando o objeto abaixo, faça os exercícios a seguir:
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
+
+//6 - Acesse as chaves nome, sobrenome e titulo, que está dentro da chave livrosFavoritos, e faça um console.log no seguinte formato: "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'".
+// console.log("O livro favorito de " + leitor.nome + " " + leitor.sobrenome + " se chama " + leitor.livrosFavoritos[0].titulo);
+
+//7-Adicione um novo livro favorito na chave livrosFavoritos, que é um array. Atribua a esta chave um objeto contendo as seguintes
+leitor.livrosFavoritos.push({titulo: 'Harry Potter e o Prisioneiro de Azkaban', autor: 'JK Rowling', editora: 'Rocco'});
+// console.log(leitor.livrosFavoritos[1]);
+
+//8 - Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: "Julia tem 2 livros favoritos".
+console.log(leitor.nome + " tem " + leitor.livrosFavoritos.length + " livros favoritos");
