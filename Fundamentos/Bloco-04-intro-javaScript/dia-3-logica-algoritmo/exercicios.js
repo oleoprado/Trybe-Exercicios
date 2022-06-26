af1eef21e08478df9274623846fd70e24840fe75
Leonardo Prado <leonardo.rprado@gmail.com>
2022-06-26 12:22:32 +0100

    configurando espaço: 2 e identando código

--- Fundamentos/Bloco-04-intro-javaScript/dia-3-logica-algoritmo/exercicios.js
+++ Fundamentos/Bloco-04-intro-javaScript/dia-3-logica-algoritmo/exercicios.js
@@ -4,13 +4,6 @@ let fatorial = 10;
 let resultado = fatorial;
 
 for (let i = 9; i > 0; i -= 1) {
-    resultado *= i;
+  resultado *= i;
 }
 console.log(resultado)
-// ou desta forma
-// let fatorial = 10;
-// let resultado = fatorial;
-// for (let i = 1; i < 10; i += 1) {
-//     resultado *= i;
-// }
-// console.log(resultado)
