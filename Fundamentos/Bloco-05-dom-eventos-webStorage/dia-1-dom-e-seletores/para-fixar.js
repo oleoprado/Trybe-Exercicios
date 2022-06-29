3067599fae7973c335010b780ecc15d8fa81ac9b
Leonardo Prado <leonardo.rprado@gmail.com>
2022-06-29 16:46:09 +0100

    finalizado exercicio para-fixar Seletores

--- Fundamentos/Bloco-05-dom-eventos-webStorage/dia-1-dom-e-seletores/para-fixar.js
+++ Fundamentos/Bloco-05-dom-eventos-webStorage/dia-1-dom-e-seletores/para-fixar.js
@@ -1,9 +1,12 @@
+const header = document.getElementById('header-container');
+header.style.backgroundColor = 'green';
+
 const emergency = document.getElementsByClassName('emergency-tasks');
 emergency[0].style.backgroundColor = "#e85d04";
 const h3emergency = document.querySelectorAll('.emergency-h3');
 for (h3 of h3emergency) {
   h3.style.backgroundColor = '#ffba08';
-  h3.style.color = 'black';
+  // h3.style.color = 'black';
 }
 
 const noEmergency = document.getElementsByClassName('no-emergency-tasks');
@@ -11,5 +14,8 @@ noEmergency[0].style.backgroundColor = "#ffba08";
 const h3noEmergency = document.querySelectorAll('.no-emergency-h3');
 for (h3 of h3noEmergency) {
   h3.style.backgroundColor = '#e85d04';
-  h3.style.color = 'black';
+  // h3.style.color = 'black';
 }
+
+const footer = document.getElementById('footer-container');
+footer.style.backgroundColor = 'green';
