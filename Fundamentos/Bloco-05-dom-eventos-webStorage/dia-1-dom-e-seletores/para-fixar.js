5e11ed25361334a267d75e674b375bd6b4e5537f
Leonardo Prado <leonardo.rprado@gmail.com>
2022-06-29 02:44:43 +0100

    iniciando manipulação do DOM do exercicio para fixar conteudo

--- Fundamentos/Bloco-05-dom-eventos-webStorage/dia-1-dom-e-seletores/para-fixar.js
+++ Fundamentos/Bloco-05-dom-eventos-webStorage/dia-1-dom-e-seletores/para-fixar.js
@@ -1,1 +1,15 @@
-const header = document.getElementById('header-container').style.color = #fc0;
+const emergency = document.getElementsByClassName('emergency-tasks');
+emergency[0].style.backgroundColor = "#e85d04";
+const h3emergency = document.querySelectorAll('.emergency-h3');
+for (h3 of h3emergency) {
+  h3.style.backgroundColor = '#ffba08';
+  h3.style.color = 'black';
+}
+
+const noEmergency = document.getElementsByClassName('no-emergency-tasks');
+noEmergency[0].style.backgroundColor = "#ffba08";
+const h3noEmergency = document.querySelectorAll('.no-emergency-h3');
+for (h3 of h3noEmergency) {
+  h3.style.backgroundColor = '#e85d04';
+  h3.style.color = 'black';
+}
