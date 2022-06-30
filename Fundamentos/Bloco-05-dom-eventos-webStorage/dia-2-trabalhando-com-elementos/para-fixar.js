f4e33cc33b687087842b14cdea01411d8a004a41
Leonardo Prado <leonardo.rprado@gmail.com>
2022-06-30 11:43:36 +0100

    refatorando codigo js, armazenando document.getElement em constantes

---
+++ Fundamentos/Bloco-05-dom-eventos-webStorage/dia-2-trabalhando-com-elementos/para-fixar.js
@@ -0,0 +1,14 @@
+//BUSCANDO ELEMENTOS
+const ondeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+ondeVoceEsta.parentNode.style.backgroundColor = 'green';
+
+const primeiroFilho = document.getElementById('primeiroFilho');
+primeiroFilho.innerText = 'Mussum Ipsum, cacilds vidis litro abertis. Nec orci ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum felis.Admodum accumsan disputationi eu sit. Vide electram sadipscing et per.Sapien in monti palavris qui num significa nadis i pareci latim.Diuretics paradis num copo é motivis de denguis.'
+document.getElementById('pai').firstElementChild;
+ondeVoceEsta.previousElementSibling;
+ondeVoceEsta.nextSibling;
+ondeVoceEsta.nextElementSibling;
+document.getElementById('pai').childNodes[5];
+
+//CRIANDO ELEMENTOS
+//1 
