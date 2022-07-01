a7af782104002454f0fb8acac2dce3159682956d
Leonardo Prado <leonardo.rprado@gmail.com>
2022-07-01 14:52:47 +0100

    resolvido requisito 1 do exercicio do dia 5.3

---
+++ Fundamentos/Bloco-05-dom-eventos-webStorage/dia-3-eventos/exercicio/main.js
@@ -0,0 +1,43 @@
+function createDaysOfTheWeek() {
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDaysList = document.querySelector('.week-days');
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
+  };
+};
+
+createDaysOfTheWeek();
+
+//1
+function createDecemberDays() {
+  const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const decemberDays = document.querySelector('#days');
+
+  for (let index = 0; index < decemberDaysList.length; index += 1) {
+    const day = decemberDaysList[index];
+    const dayList = document.createElement('li');
+    dayList.innerHTML = day;
+
+    if (day === 24 || day === 31) {
+      dayList.className = 'day holiday';
+      decemberDays.appendChild(dayList);
+    } else if (day === 4 || day === 11 || day === 18) {
+      dayList.className = 'day friday';
+      decemberDays.appendChild(dayList);
+    } else if (day === 25) {
+      dayList.className = 'day holiday friday';
+      decemberDays.appendChild(dayList);
+    } else {
+      dayList.className = 'day'
+      decemberDays.appendChild(dayList);
+    }
+  }
+}
+createDecemberDays();
+
+//2 
