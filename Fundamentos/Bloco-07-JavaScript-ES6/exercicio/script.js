58528b0c6c25bb793877c5258b09c839f7836f21
Leonardo Prado <leonardo.rprado@gmail.com>
2022-07-15 00:04:36 +0100

    realizado requisito 1 do primeiro exercicio

---
+++ Fundamentos/Bloco-07-JavaScript-ES6/exercicio/script.js
@@ -0,0 +1,31 @@
+/* 1 - 🚀 Altere as variáveis para respeitarem o escopo em que foram declaradas.
+
+Modifique a estrutura da função para que ela seja uma arrow function;
+Modifique as variáveis para que respeitem o escopo onde estão declaradas;
+Modifique as concatenações para template literals.*/
+
+const testingScope = (escopo) => {
+  if (escopo === true) {
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+    // console.log(ifScope);
+  } else {
+    const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    // console.log(elseScope);
+  }
+  // console.log(`${ifScope} o que estou fazendo aqui ? :O`); // Se necessário esta linha pode ser removida.
+}
+
+testingScope(true);
+
+/* 2 - 🚀 Crie uma função que retorne um array em ordem crescente.
+
+Crie uma função que faça com que o array oddsAndEvens fique na ordem crescente;
+Utilize o console.log e template literals para retornar a frase: 'Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!'.
+Utilize template literals para que a chamada console.log(<seu código>oddsAndEvens<seu código>); retorne a frase "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
+Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort(). Spoiler: É possível realizar uma função que ordene qualquer array de números.*/
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+const ordemCrescente = () => oddsAndEvens.sort();
+
+console.log(oddsAndEvens.sort()); // será necessário alterar essa linha 😉
