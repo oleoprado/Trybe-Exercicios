c8ad268435fb3bae242bbbf107dc902055b2f174
Leonardo Prado <leonardo.rprado@gmail.com>
2022-07-15 20:09:00 +0100

    fixando aprendizado sobre throw, try e catch e sobre objetos: Object.keys, Object.values, Object.entries e Object.assign

---
+++ Fundamentos/Bloco-07-JavaScript-ES6/dia-2-fluxo-de-excecao-e-objetos/para-fixar/script.js
@@ -0,0 +1,116 @@
+// THROW / TRY / CAT
+/* 
+const verifyIsNumber = (value1, value2) => {
+  if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+    throw new Error('Os valores devem ser numéricos');
+  }
+};
+
+const sum = (value1, value2) => {
+  try {
+    verifyIsNumber(value1, value2);
+    return value1 + value2;
+  } catch (error) {
+    return error.message;
+  }
+};
+
+console.log(sum(2, '3')); */
+
+// Objetos
+
+// const coolestTvShow = {
+//   name: "BoJack Horseman",
+//   genre: "adult animation",
+//   createdBy: "Raphael Bob-Waksberg",
+//   favoriteCharacter: "Princess Carolyn",
+//   quote: "Princess Carolyn always lands on her feet.",
+//   seasons: 6,
+// };
+
+// for (const property in coolestTvShow) {
+//   console.log(property);
+// }
+
+console.log(Object.keys(coolestTvShow));
+
+// [ 'name', 'genre', 'createdBy', 'favoriteCharacter', 'quote', 'seasons' ]
+
+console.log(Object.values(['leonardo', 'juliana', 'ana']))
+
+const student1 = {
+  Html: 'Muito Bom',
+  Css: 'Bom',
+  JavaScript: 'Ótimo',
+  SoftSkills: 'Ótimo',
+};
+
+const student2 = {
+  Html: 'Bom',
+  Css: 'Ótimo',
+  JavaScript: 'Ruim',
+  SoftSkills: 'Ótimo',
+  Git: 'Bom', // chave adicionada
+};
+
+const listSkills = (student) => {
+  const arrayOfSkills = Object.keys(student);
+  for (const index in arrayOfSkills) {
+    console.log(`Tech: ${arrayOfSkills[index]} - Nível: ${student[arrayOfSkills[index]]}`);
+  }
+};
+
+// console.log('Estudante 1');
+// listSkills(student1);
+
+// console.log('Estudante 2');
+// listSkills(student2);
+
+const student = {
+  Html: 'Muito Bom',
+  Css: 'Bom',
+  JavaScript: 'Ótimo',
+  SoftSkill: 'Ótimo',
+};
+
+const listSkillsValuesWithValues = (student) => Object.values(student);
+console.log(listSkillsValuesWithValues(student));
+
+const coolestTvShow = {
+  name: "BoJack Horseman",
+  genre: "adult animation",
+  createdBy: "Raphael Bob-Waksberg",
+  favoriteCharacter: "Princess Carolyn",
+  quote: "Princess Carolyn always lands on her feet.",
+  seasons: 6,
+};
+
+console.log(Object.entries(coolestTvShow));
+
+const países = {
+  França: 'Paris',
+  Brasil: 'Brasília',
+  Espanha: 'Madrid',
+  Portugal: 'Lisboa',
+};
+const pairKeyValue = Object.entries(países);
+console.log(pairKeyValue);
+
+const person = {
+  name: 'Alberto',
+  lastName: 'Gomes',
+  age: 20,
+};
+
+const info = {
+  age: 23,
+  job: 'engenheiro',
+};
+
+const family = {
+  children: ['Maria', 'João'],
+  wife: 'Ana',
+};
+
+Object.assign(person, info, family)
+console.log(person)
