287399058ad581bcc48efc73b7890bd2e39bf11c
Leonardo Prado <leonardo.rprado@gmail.com>
2022-07-21 14:03:58 +0100

    praticando HOFs com a função calculator

--- Fundamentos/Bloco-08-higher-order-functions-jsES6/dia-01-introducao/para-fixar/para-fixar.js
+++ Fundamentos/Bloco-08-higher-order-functions-jsES6/dia-01-introducao/para-fixar/para-fixar.js
@@ -4,6 +4,18 @@ const dormir = () => 'Partiu dormir!!';
 
 const doingThings = (func) => func();
 
-console.log(doingThings(acordando));
-console.log(doingThings(cafe));
-console.log(doingThings(dormir));
+// console.log(doingThings(acordando));
+// console.log(doingThings(cafe));
+// console.log(doingThings(dormir));
+
+const sum = (num1, num2) => num1 + num2; 
+const sub = (num1, num2) => num1 - num2; 
+const div = (num1, num2) => num1 / num2; 
+const mult = (num1, num2) => num1 * num2; 
+
+const calculator = (func) => func(10, 5);
+
+console.log(calculator(sum));
+console.log(calculator(sub));
+console.log(calculator(div));
+console.log(calculator(mult));
