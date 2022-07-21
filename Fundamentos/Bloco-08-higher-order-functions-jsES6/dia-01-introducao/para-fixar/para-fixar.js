102091ac28cc38afefd305995d382b32fb583d9d
Leonardo Prado <leonardo.rprado@gmail.com>
2022-07-21 13:59:17 +0100

    realizado requisito 4, desenvolvido a HOF solicitado

--- Fundamentos/Bloco-08-higher-order-functions-jsES6/dia-01-introducao/para-fixar/para-fixar.js
+++ Fundamentos/Bloco-08-higher-order-functions-jsES6/dia-01-introducao/para-fixar/para-fixar.js
@@ -2,3 +2,8 @@ const acordando = () => 'Acordando!!';
 const cafe = () => 'Bora tomar café!!';
 const dormir = () => 'Partiu dormir!!';
 
+const doingThings = (func) => func();
+
+console.log(doingThings(acordando));
+console.log(doingThings(cafe));
+console.log(doingThings(dormir));
