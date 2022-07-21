2093f3a0e60984caed64e3ea49a2433f79a9ac75
Leonardo Prado <leonardo.rprado@gmail.com>
2022-07-21 16:23:54 +0100

    Implementada função HOF e finalizado requisito 1 do dia 8.1

--- Fundamentos/Bloco-08-higher-order-functions-jsES6/dia-01-introducao/exercicios/exercicios.js
+++ Fundamentos/Bloco-08-higher-order-functions-jsES6/dia-01-introducao/exercicios/exercicios.js
@@ -1,14 +1,5 @@
 /* 1 - Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada. Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respectivo id. A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um email no formato nome_da_pessoa@trybe.com. */
 
-const newEmployees = () => {
-  const employees = {
-    id1: '', // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-    id2: '', // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-    id3: '', // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
-  }
-  return employees;
-};
-
 const nomeEmail = (nomeCompleto) => {
   const funcionario = {
     Nome: nomeCompleto,
@@ -16,9 +7,16 @@ const nomeEmail = (nomeCompleto) => {
   }
   return funcionario;
 }
-console.log(nomeEmail('Leonardo Prado'));
+const newEmployees = (nomeEmail) => {
+  const employees = {
+    id1: nomeEmail('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: nomeEmail('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: nomeEmail('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  }
+  return employees;
+};
+
+console.log(newEmployees(nomeEmail));
+// console.log(nomeEmail('Leonardo Prado'));
 // console.log(newEmployees());
 
-// const teste = 'leonardo augusto ribeiro prado';
-
-// console.log(teste.replace(/ /g, '_'));
