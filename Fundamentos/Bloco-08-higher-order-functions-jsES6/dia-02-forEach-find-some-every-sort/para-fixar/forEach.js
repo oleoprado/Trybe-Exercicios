a5f9686931cde1bdd5802fdbaecc13baeb19488e
Leonardo Prado <leonardo.rprado@gmail.com>
2022-07-22 20:04:06 +0100

    realizado requisito 1 do exercicio para-praticar de forEach

--- Fundamentos/Bloco-08-higher-order-functions-jsES6/dia-02-forEach-find-some-every-sort/para-fixar/forEach.js
+++ Fundamentos/Bloco-08-higher-order-functions-jsES6/dia-02-forEach-find-some-every-sort/para-fixar/forEach.js
@@ -14,3 +14,36 @@ listaDeAprovados.forEach((item, index, array) => {
   console.log(`sua posição no processo foi: ${index}`);
   console.log(`a quantidade de pessoas no processo seletivo é: ${array.length}`);
 });
+
+////////////////////////////////////////////
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const multiplicaPor2 = (elemento) => {
+  console.log(`${elemento} * 2: ${elemento * 2}`);
+}
+numbers.forEach(multiplicaPor2);
+
+//////////////////////////////////////////
+const names = ['Bianca', 'Camila', 'Fernando', 'Ana Roberta'];
+
+const convertToUpperCase = (name, index) => {
+  names[index] = name.toUpperCase();
+};
+names.forEach(convertToUpperCase);
+console.log(names);
+////////////////////////////////////////
+// PARA FIXAR
+
+// 1 - Use o método forEach chamando a callback showEmailList para apresentar os emails
+const emailListInData = [
+  'roberta@email.com',
+  'paulo@email.com',
+  'anaroberta@email.com',
+  'fabiano@email.com',
+];
+
+const showEmailList = (email) => {
+  console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
+};
+
+emailListInData.forEach(showEmailList);
