ed9cd9c2f9c149096f5f32da081462277e0cb835
Leonardo Prado <leonardo.rprado@gmail.com>
2022-07-24 12:07:40 +0100

    finalizado requisito 1 do para-fixar sobre some e every

--- Fundamentos/Bloco-08-higher-order-functions-jsES6/dia-02-forEach-find-some-every-sort/some-every.js
+++ Fundamentos/Bloco-08-higher-order-functions-jsES6/dia-02-forEach-find-some-every-sort/some-every.js
@@ -4,3 +4,27 @@ const verifyFirstLetter = (letter, names) => names.some((name) => name[0] === le
 
 console.log(verifyFirstLetter('J', listNames)); // true
 console.log(verifyFirstLetter('x', listNames)); // false
+
+//////////////////////////////////////////////////////////
+
+const grades = {
+  portugues: 'Aprovado',
+  matematica: 'Reprovado',
+  ingles: 'Aprovado',
+};
+
+// console.log(Object.values(grades));
+
+const verifyGrades = (studentGrades) => (Object.values(studentGrades).every((grade) => grade === 'Aprovado')
+);
+
+console.log(verifyGrades(grades));
+//////////////////////////////////////////////////////////
+// 1 - Escreva uma função que dado um array de nomes e um nome retorne true se ele estiver contido e caso contrário, retorne false, use some;
+const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+
+const hasName = (arr, name) => {
+  return arr.some((nome) => nome === name );
+}
+
+console.log(hasName(names, 'Mateus'))
