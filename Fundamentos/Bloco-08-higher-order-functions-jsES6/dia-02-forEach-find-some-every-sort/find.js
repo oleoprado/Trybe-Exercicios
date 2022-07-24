291dcc5e88a2c3f292d71e9f85bee16676a07943
Leonardo Prado <leonardo.rprado@gmail.com>
2022-07-24 11:01:18 +0100

    realizado requisito 1 do para fixar sobre find()

--- Fundamentos/Bloco-08-higher-order-functions-jsES6/dia-02-forEach-find-some-every-sort/find.js
+++ Fundamentos/Bloco-08-higher-order-functions-jsES6/dia-02-forEach-find-some-every-sort/find.js
@@ -2,12 +2,12 @@
 
 const verifyEven = (number) => number % 2 === 0;
 
-// const isEven = numbers.find(verifyEven);
+const isEven = numbers.find(verifyEven);
 
 // console.log(isEven); // 30
 
-// console.log(verifyEven(9)); // False
-console.log(verifyEven(14)); // True
+console.log(verifyEven(9)); // False
+// console.log(verifyEven(14)); // True
 
 // Outra forma de ser realizada sem a necessidade de criar uma nova função.
 const isEven2 = numbers.find((number) => number % 2 === 0);
@@ -17,3 +17,9 @@ console.log(isEven2); // 30
 ////////////
 // 1 - Utilize o find para retornar o primeiro número do array que é divisível por 3 e 5, caso ele exista:
 const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const findDivisibleBy3And5 = () => {
+  return numbers.find((number) => number % 3 === 0 && number % 5 === 0);
+};
+
+console.log(findDivisibleBy3And5());
