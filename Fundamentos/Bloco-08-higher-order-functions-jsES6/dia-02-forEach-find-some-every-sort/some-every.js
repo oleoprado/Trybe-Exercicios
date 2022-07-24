e8133fbd53d9ac5fb36d088680f84f1e918afc06
Leonardo Prado <leonardo.rprado@gmail.com>
2022-07-24 12:39:32 +0100

    finalizado exercicio para fixar sobre som e every ()

--- Fundamentos/Bloco-08-higher-order-functions-jsES6/dia-02-forEach-find-some-every-sort/some-every.js
+++ Fundamentos/Bloco-08-higher-order-functions-jsES6/dia-02-forEach-find-some-every-sort/some-every.js
@@ -20,6 +20,7 @@ const verifyGrades = (studentGrades) => (Object.values(studentGrades).every((gra
 
 console.log(verifyGrades(grades));
 //////////////////////////////////////////////////////////
+
 // 1 - Escreva uma função que dado um array de nomes e um nome retorne true se ele estiver contido e caso contrário, retorne false, use some;
 const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
 
@@ -28,3 +29,18 @@ const hasName = (arr, name) => {
 }
 
 console.log(hasName(names, 'Mateus'))
+//////////////////////////////////////////////////////////
+
+// 2 - Escreva uma função que dado um array de pessoas e uma idade mínima retorne true se todas tiverem a idade maior ou igual a mínima e caso contrário false, use every;
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+const verifyAges = (arr, minimumAge) => {
+  return arr.every((pessoa) => pessoa.age >= minimumAge);
+}
+
+console.log(verifyAges(people, 18));
