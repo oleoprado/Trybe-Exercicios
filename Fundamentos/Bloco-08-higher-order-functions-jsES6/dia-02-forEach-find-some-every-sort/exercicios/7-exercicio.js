7eca4b5dd9f7898d615017683cfdd9319d4e06dd
Leonardo Prado <leonardo.rprado@gmail.com>
2022-07-25 19:18:21 +0100

    refatorado requisito 7 do exercicio do dia 8.2

--- Fundamentos/Bloco-08-higher-order-functions-jsES6/dia-02-forEach-find-some-every-sort/exercicios/7-exercicio.js
+++ Fundamentos/Bloco-08-higher-order-functions-jsES6/dia-02-forEach-find-some-every-sort/exercicios/7-exercicio.js
@@ -7,4 +7,6 @@ function authorUnique() {
   // escreva seu código aqui
   return books.every((book) => !books.some((bookSome) => (bookSome.author.birthYear === book.author.birthYear) && (bookSome.author.name !== book.author.name)));
 }
+
 console.log(authorUnique());
+
