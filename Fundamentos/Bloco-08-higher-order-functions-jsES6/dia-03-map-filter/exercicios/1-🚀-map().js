2848512556f60772a1098b59e638257188a1538f
Leonardo Prado <leonardo.rprado@gmail.com>
2022-07-25 19:32:31 +0100

    resolvido requisito 1 do exercicio do dia 8.3

--- Fundamentos/Bloco-08-higher-order-functions-jsES6/dia-03-map-filter/exercicios/1-🚀-map().js
+++ Fundamentos/Bloco-08-higher-order-functions-jsES6/dia-03-map-filter/exercicios/1-🚀-map().js
@@ -1,2 +1,19 @@
 const books = require('./books.js')
 
+// 🚀 1 - Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA
+// Dica: Use a função map
+const expectedResult = [
+  'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
+  'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
+  'Fundação - Ficção Científica - Isaac Asimov',
+  'Duna - Ficção Científica - Frank Herbert',
+  'A Coisa - Terror - Stephen King',
+  'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+];
+
+function formatedBookNames() {
+  // escreva seu código aqui
+  const arrString = books.map((elemento) => `${elemento.name} - ${elemento.genre} - ${elemento.author.name}`);
+  return arrString;
+}
+console.log(formatedBookNames());
