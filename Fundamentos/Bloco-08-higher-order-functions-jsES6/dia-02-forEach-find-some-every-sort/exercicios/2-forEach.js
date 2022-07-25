b32dce1f1178e36ce1284f97ea72b4f29514df71
Leonardo Prado <leonardo.rprado@gmail.com>
2022-07-25 11:50:15 +0100

    realizado exercicio2 sobre forEach do exercicio do dia 8.2

--- Fundamentos/Bloco-08-higher-order-functions-jsES6/dia-02-forEach-find-some-every-sort/exercicios/2-forEach.js
+++ Fundamentos/Bloco-08-higher-order-functions-jsES6/dia-02-forEach-find-some-every-sort/exercicios/2-forEach.js
@@ -60,14 +60,18 @@ const books = [
     releaseYear: 1928,
   },
 ];
-
 // 2 - Retorne o nome do livro de menor nome.
 // Dica: use a função forEach.
 
 function smallerName() {
   let nameBook;
   // escreva aqui o seu código
-
+  books.forEach((livro) => {
+    if (!nameBook || livro.name.length < nameBook.length) {
+      nameBook = livro.name;
+    }
+  })
   // Variável nameBook que receberá o valor do menor nome;
   return nameBook;
 }
+console.log(smallerName());
