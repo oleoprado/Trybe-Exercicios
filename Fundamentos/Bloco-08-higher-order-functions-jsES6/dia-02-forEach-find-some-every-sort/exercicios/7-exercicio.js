616d89049c3a60f613b934412413c3b0c588a8f8
Leonardo Prado <leonardo.rprado@gmail.com>
2022-07-25 14:10:46 +0100

    realizado requisito 7 e finalizado exercicio do dia 8.2

---
+++ Fundamentos/Bloco-08-higher-order-functions-jsES6/dia-02-forEach-find-some-every-sort/exercicios/7-exercicio.js
@@ -0,0 +1,10 @@
+const books = require('./books.js');
+
+// 7 - Faça uma função que retorne true, caso nenhum author tenha nascido no mesmo ano, e false, caso contrário.
+const expectedResult = false;
+
+function authorUnique() {
+  // escreva seu código aqui
+  return books.every((book) => !books.some((bookSome) => (bookSome.author.birthYear === book.author.birthYear) && (bookSome.author.name !== book.author.name)));
+}
+console.log(authorUnique());
