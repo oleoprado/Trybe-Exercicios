329e2aeca05c98d8ce7afe93b5e3bde0a9042ed3
Leonardo Prado <leonardo.rprado@gmail.com>
2022-07-25 10:51:18 +0100

    realizado exercicio para-fixar sobre o metodo sort()

--- Fundamentos/Bloco-08-higher-order-functions-jsES6/dia-02-forEach-find-some-every-sort/sort.js
+++ Fundamentos/Bloco-08-higher-order-functions-jsES6/dia-02-forEach-find-some-every-sort/sort.js
@@ -49,4 +49,21 @@ gamers.sort((a,b) => {
 })
 console.log(gamers);
 //////////////////////////////////////
+// PARA FIXAR
 
+// 1 - 1 - Utilize a sort para ordenar o array pela idade das pessoas em ordem crescente.
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+people.sort((a,b) => a.age > b.age ? 1 : -1);
+
+// 2 - Modifique o sort do exercício anterior para que ordene o array pela idade das pessoas em ordem decrescente.
+
+people.sort((a,b) => a.age < b.age ? 1 : -1);
+
+console.log(people);
