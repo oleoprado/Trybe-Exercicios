daa0de9cf8e25d5883f42ea8921f1d4e80e1967b
Leonardo Prado <leonardo.rprado@gmail.com>
2022-07-26 11:30:59 +0100

    finalizado requisito 5 do exercicio do dia 8.3

--- Fundamentos/Bloco-08-higher-order-functions-jsES6/dia-03-map-filter/exercicios/5-🚀 -filter_sort.js
+++ Fundamentos/Bloco-08-higher-order-functions-jsES6/dia-03-map-filter/exercicios/5-🚀 -filter_sort.js
@@ -1,1 +1,15 @@
 const books = require('./books.js');
+// 🚀 5 - Crie um array em ordem alfabética apenas com os nomes de todas as pessoas autoras de ficção científica ou fantasia
+const expectedResult = [
+  'Frank Herbert',
+  'George R. R. Martin',
+  'Isaac Asimov',
+  'J. R. R. Tolkien',
+];
+
+function fantasyOrScienceFictionAuthors() {
+  // escreva seu código aqui
+  const fantasiaEficcao =  books.filter((book) => book.genre.includes('Ficção Científica') || book.genre.includes('Fantasia'));
+  return fantasiaEficcao.map((book) => book.author.name).sort();
+}
+console.log(fantasyOrScienceFictionAuthors());
