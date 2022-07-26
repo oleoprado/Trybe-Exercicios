dfe5e53dc4c7001b54d960822482cd784f577979
Leonardo Prado <leonardo.rprado@gmail.com>
2022-07-26 11:09:19 +0100

    realizado requisito 4 do exercicio do dia 8.3

---
+++ Fundamentos/Bloco-08-higher-order-functions-jsES6/dia-03-map-filter/exercicios/4-🚀-filter_sort.js
@@ -0,0 +1,42 @@
+const books = require('./books.js');
+// 🚀 4 - Crie um array ordenado pelos livros com mais de 60 anos de publicação e ordene-o pelo livro mais velho.
+// Dica: use as funções filter e sort
+const expectedResult = [
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+    releaseYear: 1928,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: { name: 'Isaac Asimov', birthYear: 1920 },
+    releaseYear: 1951,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954,
+  },
+];
+
+function oldBooksOrdered() {
+  // escreva seu código aqui
+  const oldBooks = books.filter((book) => (2022 - book.releaseYear) > 60);
+  return oldBooks.sort((a,b) => a.releaseYear - b.releaseYear);
+}
+console.log(oldBooksOrdered());
+
+
+
+
+
+
+
+// let anoAtual = new Date();
+// console.log(anoAtual.getFullYear());
