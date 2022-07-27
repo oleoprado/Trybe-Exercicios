1fca8be1fd238791a7c38b72e131e1617a545c5e
Leonardo Prado <leonardo.rprado@gmail.com>
2022-07-27 19:32:27 +0100

    criado arquivo para fixar conhecimento do dia 8.5

---
+++ Fundamentos/Bloco-08-higher-order-functions-jsES6/dia-05-spread-paramRest-destructuring/para-fixar/spread-operator.js
@@ -0,0 +1,8 @@
+const numbers = [1, 2, 3];
+
+const newArray = [...numbers, 4, 5, 6];
+// console.log(newArray); // [ 1, 2, 3, 4, 5, 6 ]
+// console.log(numbers); // [ 1, 2, 3 ]
+
+/////////////////////////////////////////
+//  O spread é muito útil também quando precisamos combinar arrays em uma única estrutura
