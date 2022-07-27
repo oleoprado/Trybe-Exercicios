1edd651bbf2f90443b7ba1087124b2478cfb025b
Leonardo Prado <leonardo.rprado@gmail.com>
2022-07-27 11:52:04 +0100

    finalizado requisito 3 do exercicio do dia 8.4

--- Fundamentos/Bloco-08-higher-order-functions-jsES6/dia-04-reduce/exercicios/3-🚀 -reduce().js
+++ Fundamentos/Bloco-08-higher-order-functions-jsES6/dia-04-reduce/exercicios/3-🚀 -reduce().js
@@ -1,2 +1,10 @@
 const books = require('./books.js');
 // 🚀 3- Calcule a média de idade que as pessoas autoras tinham quando seus respectivos livros foram lançados.
+const expectedResult = 43;
+
+function averageAge() {
+  const qtdLivros = books.length;
+  const mediaIdades = books.map((book) => book.releaseYear - book.author.birthYear).reduce((acc, curr, array) => (acc + curr) , 0);
+  return (mediaIdades / qtdLivros);
+}
+console.log(averageAge()); 
