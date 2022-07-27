c9a1157dedf51348ebeb35bf3184d16355b40a89
Leonardo Prado <leonardo.rprado@gmail.com>
2022-07-27 11:54:17 +0100

    iniciado requisito 4 exercicio do dia 8.4

---
+++ Fundamentos/Bloco-08-higher-order-functions-jsES6/dia-04-reduce/exercicios/4-🚀-reduce().js
@@ -0,0 +1,16 @@
+const books = require('./books.js');
+// 🚀 4- Encontre o livro com o maior nome.
+const expectedResult = {
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  genre: 'Fantasia',
+  author: {
+    name: 'George R. R. Martin',
+    birthYear: 1948,
+  },
+  releaseYear: 1991,
+};
+
+function longestNamedBook() {
+  // escreva seu código aqui
+}
