ae998aba2ae1680580b7a2e6ea087150d6fcc8c9
Leonardo Prado <leonardo.rprado@gmail.com>
2022-07-28 11:57:33 +0100

    criado arquivo para fixar conhecimento sobre default destructuring

--- Fundamentos/Bloco-08-higher-order-functions-jsES6/dia-05-spread-paramRest-destructuring/para-fixar/array-destructuring.js
+++ Fundamentos/Bloco-08-higher-order-functions-jsES6/dia-05-spread-paramRest-destructuring/para-fixar/array-destructuring.js
@@ -10,10 +10,10 @@ const [firstCountry, secondCountry, thirdCountry, fourthCountry] = arrayCountrie
 //////////////////////////////////////////
 // PARA FIXAR 
 // 1 - Produza o mesmo resultado do código, porém utilizando o array destructuring para recolher a função e a saudação.
-const saudacoes = ['Olá', (saudacao) => console.log(saudacao)];
-saudacoes[1](saudacoes[0]); // Olá
+// const saudacoes = ['Olá', (saudacao) => console.log(saudacao)];
+// saudacoes[1](saudacoes[0]); // Olá
 // Produza o mesmo resultado acima, porém utilizando array destructuring
-const [saudacao, realizaSaudacao] = saudacoes;
+// const [saudacao, realizaSaudacao] = saudacoes;
 // console.log(realizaSaudacao(saudacao));
 
 /////////////////////////////////////////////////////
@@ -23,6 +23,11 @@ let animal = 'água';
 let bebida = 'arroz';
 
 [comida, animal, bebida] = [bebida, comida, animal];
-console.log(comida, animal, bebida); // arroz gato água
+// console.log(comida, animal, bebida); // arroz gato água
 
-// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+/////////////////////////////////////////////////////
+// 3 - array abaixo possui alguns números que não condizem com o conteúdo que ele deveria possuir. Através de array destructuring, faça com que existam apenas números pares na variável numerosPares.
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+const [,,, ...numbers] = numerosPares;
+console.log(numbers); // [6, 8, 10, 12];
+
---
+++ Fundamentos/Bloco-08-higher-order-functions-jsES6/dia-05-spread-paramRest-destructuring/para-fixar/default-destructuring.js
@@ -0,0 +1,19 @@
+const person = {
+  name: 'João',
+  lastName: 'Jr',
+  age: 34,
+};
+
+const { nationality = 'cubano'} = person;
+
+////////////////////////////////////////
+const position2d = [1.0, 2.0];
+const [x, y, z = 0] = position2d;
+
+console.log(x); // 1
+console.log(y); // 2
+console.log(z); // 0
+
+////////////////////////////////////////
+// PARA FIXAR
+// Do jeito que está, quando passamos person para a função GetNationality o retorno é João is undefined. Ajuste a função GetNationality para que a chamada GetNationality(person) retorne João is Brazilian.
