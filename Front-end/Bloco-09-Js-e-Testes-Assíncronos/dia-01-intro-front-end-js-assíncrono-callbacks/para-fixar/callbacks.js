702cc927b6d535ea930f57368c92ab6c10e526ab
Leonardo Prado <leonardo.rprado@gmail.com>
2022-08-04 17:13:25 +0100

    finalizado atividade 2 para-fixar sobre callbacks

--- Front-end/Bloco-09-Js-e-Testes-Assíncronos/dia-01-intro-front-end-js-assíncrono-callbacks/para-fixar/callbacks.js
+++ Front-end/Bloco-09-Js-e-Testes-Assíncronos/dia-01-intro-front-end-js-assíncrono-callbacks/para-fixar/callbacks.js
@@ -39,21 +39,45 @@
 
 /////////////////////////////////////////////////
 // PARA-FIXAR
-// Insira o retorno da função getUser;
+// 1- Insira o retorno da função getUser;
 // Complete a chamada da função getUser de modo que o retorno seja: "Hello! My name is Ivan Ivanovich";
 // Complete a chamada da função getUser de modo que o retorno seja: "Ivan is Russian".
+// const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+// const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+// const getUser = (param) => {
+//   const userToReturn = {
+//     firstName: "Ivan",
+//     lastName: "Ivanovich",
+//     nationality: "Russian"
+//   };
+//   // Insira o retorno da função `getUser`
+//   return param(userToReturn);
+// };
+
+// console.log(getUser(userFullName)); // complete a chamada da função getUser de modo que o retorno seja: "Hello! My name is Ivan Ivanovich"
+// console.log(getUser(userNationality)); // complete a chamada da função getUser de modo que o retorno seja: "Ivan is Russian"
+
+// 2 -  Passe, como parâmetro e como retorno, uma callback para a função getUser.
+// Insira uma callback como parâmetro da função getUser;
+// Retorne a callback passada como parâmetro na função getUser;
 const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
 const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
 
-const getUser = (param) => {
-  const userToReturn = {
-    firstName: "Ivan",
-    lastName: "Ivanovich",
-    nationality: "Russian"
-  };
-  // Insira o retorno da função `getUser`
-  return param(userToReturn);
+const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+
+const getUser = (callback) => {
+  setTimeout(() => {
+    const user = {
+      firstName: "Ivan",
+      lastName: "Ivanovich",
+      nationality: "Russian",
+    };
+    // Retorne a `callback` passada como parâmetro na função `getUser`
+    // Dica: você pode manter o `console.log()`
+    return console.log(callback(user));
+  }, delay());
 };
 
-console.log(getUser(userFullName)); // complete a chamada da função getUser de modo que o retorno seja: "Hello! My name is Ivan Ivanovich"
-console.log(getUser(userNationality)); // complete a chamada da função getUser de modo que o retorno seja: "Ivan is Russian"
+getUser(userFullName); // deve imprimir "Hello! My name is Ivan Ivanovich" depois de um certo tempo
+getUser(userNationality); // deve imprimir "Ivan is Russian" depois de um certo tempo
