04265191e1e9aea127914604689282a1801b9ee5
Leonardo Prado <leonardo.rprado@gmail.com>
2022-08-17 15:43:21 +0100

    finalizado exercicio do course 10.1

--- Front-end/Bloco-10-Intro-React/exercicio/exercicio-course/src/App.js
+++ Front-end/Bloco-10-Intro-React/exercicio/exercicio-course/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
-
-class App extends React.Component {
- render() {
+const Task = (value) => {
   return (
-    <h1>leo prado</h1>
-  )
- }
+    <li key={value}>{value}</li>
+  );
+}
+
+const compromissos = ['estudar react', 'ir ao dentista', 'aniversario final de semana', 'pagar internet', 'comprar ração', 'supermercado', 'ler um livro']
+const jsxCompromissos = compromissos.map((compromisso) => Task(compromisso));
+class App extends React.Component {
+  render() {
+    return (
+      <ul>
+        {jsxCompromissos}
+      </ul>
+    )
+  }
 }
 
 export default App;
