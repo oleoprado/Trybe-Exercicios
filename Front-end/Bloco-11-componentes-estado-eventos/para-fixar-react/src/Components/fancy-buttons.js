6f8053ed2057c65c6e27326b458448f1484dc217
Leonardo Prado <leonardo.rprado@gmail.com>
2022-08-19 22:32:09 +0100

    refatorado para-fixar

--- Front-end/Bloco-11-componentes-estado-eventos/para-fixar-react/src/Components/fancy-buttons.js
+++ Front-end/Bloco-11-componentes-estado-eventos/para-fixar-react/src/Components/fancy-buttons.js
@@ -1,32 +1,32 @@
 import React from "react";
 
 class Buttons extends React.Component {
-  constructor() {
-    super();
-    this.clickEsquerda = this.clickEsquerda.bind(this);
-    this.clickCentro = this.clickCentro.bind(this);
-    this.clickDireita = this.clickDireita.bind(this);
-  }
+  // constructor() {
+  //   super();
+  //   this.clickEsquerda = this.clickEsquerda.bind(this);
+  //   this.clickCentro = this.clickCentro.bind(this);
+  //   this.clickDireita = this.clickDireita.bind(this);
+  // }
 
   state = {
     numClickEsq: 0,
     numClickCent: 0,
     numClickDir: 0
   }
-  clickEsquerda = () => this.setState((estadoAnterior, _props) => ({numClickEsq: estadoAnterior.numClickEsq + 1}));
-  clickCentro = () => this.setState((estadoAnterior, _props) => ({numClickCent: estadoAnterior.numClickCent + 1}));
-  clickDireita = () => this.setState((estadoAnterior, _props) => ({numClickDir: estadoAnterior.numClickDir + 1}));
+
+  clickEsquerda = () => this.setState((estadoAnterior) => ({numClickEsq: estadoAnterior.numClickEsq + 1}));
+  clickCentro = () => this.setState((estadoAnterior) => ({numClickCent: estadoAnterior.numClickCent + 1}));
+  clickDireita = () => this.setState((estadoAnterior) => ({numClickDir: estadoAnterior.numClickDir + 1}));
   
   render() {
     // console.log(this);
     // const { esquerda, centro, direita } = this.props;
-    const { numClickEsq, numClickCent, numClickDir } = this.props;
+    // const { numClickEsq, numClickCent, numClickDir } = this.props;
     return (
       <span>
-        {console.log(this.state)};
-        <button onClick={ this.clickEsquerda }>{ numClickEsq }</button>
-        <button onClick={ this.clickCentro }>{ numClickCent }</button>
-        <button onClick={ this.clickDireita }>{ numClickDir }</button>
+        <button onClick={ this.clickEsquerda }>{ this.state.numClickEsq}</button>
+        <button onClick={ this.clickCentro }>{ this.state.numClickCent }</button>
+        <button onClick={ this.clickDireita }>{ this.state.numClickDir }</button>
       </span>
     )
   }
