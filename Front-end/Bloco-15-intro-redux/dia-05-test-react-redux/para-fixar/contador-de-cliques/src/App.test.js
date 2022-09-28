e2ce0fca698a4cb5be9eb5bbe215a70615cb7d9d
Leonardo Prado <leonardo.rprado@gmail.com>
2022-09-28 17:34:36 +0100

    finalizado para-fixar 15.5

---
+++ Front-end/Bloco-15-intro-redux/dia-05-test-react-redux/para-fixar/contador-de-cliques/src/App.test.js
@@ -0,0 +1,36 @@
+// src/App.test.js
+import React from 'react';
+import { Provider } from 'react-redux';
+import { render, cleanup, screen } from '@testing-library/react';
+import App from './App';
+import { legacy_createStore as createStore, combineReducers } from 'redux';
+import clickReducer from './reducers';
+
+const renderWithRedux = (
+  component,
+  {
+    initialState,
+    store = createStore(combineReducers({ clickReducer }), initialState),
+  } = {}
+) => ({
+  ...render(<Provider store={store}>{component}</Provider>),
+  store,
+});
+
+describe('testing clicks', () => {
+  beforeEach(cleanup);
+  test('the page should have a button and a text 0', () => {
+    renderWithRedux(<App />);
+    const buttonAdicionar = screen.queryByText('Clique aqui');
+
+    expect(buttonAdicionar).toBeInTheDocument();
+    expect(screen.getByText('0')).toBeInTheDocument();
+  });
+
+  // Utilizaremos o seletor `getByText` para retornar o nó correspondente a uma consulta de texto
+  test('a click in a button should increment the value of clicks', () => {
+    renderWithRedux(<App />, { initialState: { clickReducer: { counter: 5 } } });
+
+    expect(screen.getByText('5')).toBeInTheDocument();
+  });
+});
