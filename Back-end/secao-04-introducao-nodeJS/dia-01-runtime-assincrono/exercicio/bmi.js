22c792a061fd6abc957fd901d1b50ca6d21bb7b0
Leonardo Prado <leonardo.rprado@gmail.com>
2022-12-07 14:01:40 +0000

    feat: Exercicio 5

--- Back-end/secao-04-introducao-nodeJS/dia-01-runtime-assincrono/exercicio/bmi.js
+++ Back-end/secao-04-introducao-nodeJS/dia-01-runtime-assincrono/exercicio/bmi.js
@@ -1,17 +1,42 @@
-const weight = 78;
-const height = 1.78;
+// const weight = 78;
+// const height = 1.78;
+
+const { questionFloat } = require('readline-sync');
 
 function calculatorBmi(weight, height) {
   console.log(`Weight: ${weight}, Height: ${height}`);
 
-  const bmi = weight / (height * 2);
+  const bmi = weight / (height * height);
 
   return bmi;
 }
 
 function main() {
+  const weight = questionFloat('What’ your weight?');
+  const height = questionFloat('What’ your height?');
+
   const bmi = calculatorBmi(weight, height);
 
+  switch (true) {
+    case (bmi >= 40):
+      console.log('Obesidade graus III e IV');
+      break;
+    case (bmi >= 35):
+      console.log('Obesidade grau II');
+      break;
+    case (bmi >= 30):
+      console.log('Obesidade grau I');
+      break;
+    case (bmi >= 25):
+      console.log('Acima do peso (sobrepeso)');
+      break;
+    case (bmi >= 18.5):
+      console.log('Peso normal');
+      break;
+    default:
+      console.log('Abaixo do peso (magreza)');
+      break;
+  }
   console.log(`BMI: ${bmi.toFixed(2)}`);
 }
 
