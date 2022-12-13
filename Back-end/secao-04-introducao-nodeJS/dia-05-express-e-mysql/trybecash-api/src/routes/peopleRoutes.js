0939561d6e582e29fbe678bdd2622d003e1624b9
Leonardo Prado <leonardo.rprado@gmail.com>
2022-12-13 17:42:24 +0000

    feat: Inserindo usuario no bd via metodo POST

--- Back-end/secao-04-introducao-nodeJS/dia-05-express-e-mysql/trybecash-api/src/routes/peopleRoutes.js
+++ Back-end/secao-04-introducao-nodeJS/dia-05-express-e-mysql/trybecash-api/src/routes/peopleRoutes.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const peopleDB = require('../db/peopleDB');
 
 const peopleRoutes = express.Router();
 
-peopleRoutes.post('/', (req, res) => {
+peopleRoutes.post('/', async (req, res) => {
   const person = req.body;
-  res.status(201).json(person);
+  try {
+    const [result] = await peopleDB.insert(person);
+    res.status(201).json({ message: `Pessoa cadastrada com sucesso com o id ${result.insertId}`})
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: "Ocorreu um erro ao cadastrar uma pessoa" });
+  }
 });
 
 module.exports = peopleRoutes;
