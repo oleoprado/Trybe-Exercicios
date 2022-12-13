a3c320f0d985f6de520ef38e6bc56e42e459b291
Leonardo Prado <leonardo.rprado@gmail.com>
2022-12-13 19:11:14 +0000

    feat: configurando variaveis de ambiente

--- Back-end/secao-04-introducao-nodeJS/dia-05-express-e-mysql/trybecash-api/src/server.js
+++ Back-end/secao-04-introducao-nodeJS/dia-05-express-e-mysql/trybecash-api/src/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const app = require('./app');
 const connection = require('./db/connection');
 
@@ -5,4 +6,5 @@ const PORT = 3001;
 
 app.listen(PORT, async () => {
   console.log(`API TrybeCash está sendo executado na porta ${PORT}`);
+  // console.log(`Valor da variavel de ambiente $USER: ${process.env.USER}`);
 });
