5ea55ee3987cd2a5632af19f3ebcd4dd27f510ac
Leonardo Prado <leonardo.rprado@gmail.com>
2023-02-12 16:03:16 +0000

    feat: criado classe Person - exercicio 9.2

---
+++ Back-end/secao-09-poo/dia-02-heranca-e-composicao/exercicio/src/Student.ts
@@ -0,0 +1,37 @@
+import Person from "./Person"
+
+export default class Student extends Person {
+  private _enrollment = String();
+  private _examsGrades: number[] = [];
+  private _assignmentsGrades: number[] = [];
+
+  constructor(name: string, birthDate: Date) {
+    super(name, birthDate);
+    this.enrollment = this.generateEnrollment();
+  }
+
+  get enrollment(): string { return this.enrollment; }
+  set enrollment(value: string) {
+    if (value.length < 16) throw new Error('A matricula deve possuir no mínimo 16 caracteres');
+    this._enrollment = value;
+  }
+
+  get examsGrades(): number[] { return this._examsGrades; }
+  set examsGrades(value: number[]) {
+    if (value.length > 4) throw new Error('A pessoa estudante só pode ter 4 notas de exame');
+    this._examsGrades = value;
+  }
+
+  get assignmentsGrades(): number[] { return this._assignmentsGrades; }
+  set assignmentsGrades(value: number[]) {
+    if (value.length > 2) throw new Error('A pessoa estudante só pode ter 2 notas de trabalho');
+    this._assignmentsGrades = value;
+  }
+
+  //esse método gera um id de inscrição aleatório baseado na data de cadastro da pessoa estudante
+  generateEnrollment(): string {
+    const randomStr = String(Date.now() * (Math.random() + 1)).replace(/\W/g, '');
+
+    return `STU${randomStr}`;
+  }
+}
---
+++ Back-end/secao-09-poo/dia-02-heranca-e-composicao/exercicio/src/index.ts
@@ -0,0 +1,8 @@
+import Person from "./Person";
+import Student from "./Student";
+
+const p1 = new Person('léo', new Date('1990-08-06'));
+// console.log(p1);
+
+const stu1 = new Student('Karl', new Date('1934-02-11'));
+console.log(stu1);
